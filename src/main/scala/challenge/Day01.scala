fa4009e216695f9fbae7c0e80d76fe9c0ebe4496
Matti Lupari <matti.lupari@gmail.com>
2022-12-02 11:48:10 +0200

    Formatting

--- src/main/scala/challenge/Day01.scala
+++ src/main/scala/challenge/Day01.scala
@@ -4,7 +4,13 @@ import scala.io.Source
 
 object Day01:
 
-  val input = Source.fromResource("day01.txt").mkString.split("\n\n").map(_.split("\n").map(_.toInt)).map(_.sum).sorted
+  val input = Source
+    .fromResource("day01.txt")
+    .mkString
+    .split("\n\n")
+    .map(_.split("\n").map(_.toInt))
+    .map(_.sum)
+    .sorted
 
   def partOne(): Int = input.last
   def partTwo(): Int = input.takeRight(3).sum
--- src/main/scala/challenge/Day02.scala
+++ src/main/scala/challenge/Day02.scala
@@ -4,11 +4,10 @@ import scala.io.Source
 
 object Day02:
 
-  val order1 = List("B X", "C Y", "A Z", "A X", "B Y", "C Z", "C X", "A Y", "B Z").zipWithIndex.toMap
-  val order2 = List("B X", "C X", "A X", "A Y", "B Y", "C Y", "C Z", "A Z", "B Z").zipWithIndex.toMap  
+  val score1 = Seq("B X", "C Y", "A Z", "A X", "B Y", "C Z", "C X", "A Y", "B Z").zipWithIndex.toMap
+  val score2 = Seq("B X", "C X", "A X", "A Y", "B Y", "C Y", "C Z", "A Z", "B Z").zipWithIndex.toMap
 
-  val input = Source.fromResource("day02.txt").getLines().toList
+  val input = Source.fromResource("day02.txt").getLines.toList
 
-  def partOne(): Int = input.map(order1(_) + 1).sum
-  def partTwo(): Int = input.map(order2(_) + 1).sum
-
+  def partOne(): Int = input.map(score1(_) + 1).sum
+  def partTwo(): Int = input.map(score2(_) + 1).sum
