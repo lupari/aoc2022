1fe8ef925c7b046aaf1a55854fa5d7fc8ec0391a
Matti Lupari <matti.lupari@gmail.com>
2022-12-07 11:37:40 +0200

    Day 7

--- src/main/scala/lib/MapExtensions.scala
+++ src/main/scala/lib/MapExtensions.scala
@@ -7,3 +7,8 @@ object MapExtensions:
       val (existing, absent) = other.partition(o => map.contains(o._1))
       val map2               = map ++ absent.map(kv => kv._1 -> Seq(kv._2))
       existing.foldLeft(map2)((acc, xs) => acc + (xs._1 -> (acc(xs._1) :+ xs._2)))
+
+  extension [A, B: Numeric](map: Map[A, B])
+    def addOrUpdate(key: A, value: B): Map[A, B] =
+      if map.contains(key) then map + (key -> (summon[Numeric[B]].plus(map(key), value)))
+      else map + (key                      -> value)
